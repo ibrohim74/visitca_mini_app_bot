d80b016fac6fa24fb10088f18d6951e2b4f023a5
ibrohim74 <xibroxim@gmail.com>
2024-04-09 09:31:59 +0500

    test1

--- src/page/home_page/API/homePageAPI.js
+++ src/page/home_page/API/homePageAPI.js
@@ -4,7 +4,7 @@ export const GetCurrentUser = ()=>{
     const token = new URLSearchParams(window.location.search);
     try {
         const JWT = jwtDecode(token)
-        const res = $host.post(`user/${JWT.userId}`)
+        const res = $host.get(`user/${JWT.userId}`)
 
         return res
     }catch (e){
