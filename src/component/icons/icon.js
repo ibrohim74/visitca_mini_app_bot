ce3c27dc7fad37946e9b9939c25ebf78e6b58389
ibrohim74 <xibroxim@gmail.com>
2024-04-09 22:52:14 +0500

    test1

--- src/component/icons/icon.js
+++ src/component/icons/icon.js
@@ -1,10 +1,12 @@
 import {ReactComponent as Logo} from "./logo.svg";
 import {ReactComponent as UserDefolt} from "./userDefolt.svg";
 import {ReactComponent as Question} from "./question.svg";
+import {ReactComponent as ArrowLeft} from "./arrowLeft.svg";
 
 
 export const Icon = {
     Logo,
     UserDefolt,
-    Question
+    Question,
+    ArrowLeft
 }
--- src/page/requests_page/requestsPage.js
+++ src/page/requests_page/requestsPage.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import BackButton from "../../component/back_button/backButton";
+import {BOOKINGS_PAGE, HOME_PAGE} from "../../utils/consts";
 
 const RequestsPage = () => {
     return (
         <div>
+            <BackButton url={BOOKINGS_PAGE}/>
             request
         </div>
     );
