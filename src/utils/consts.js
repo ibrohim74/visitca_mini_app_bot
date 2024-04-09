81c0b1de8a2a2d0813212cd65c05607ffcc3d141
ibrohim74 <xibroxim@gmail.com>
2024-04-10 01:11:18 +0500

    test1

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,6 +1,6 @@
 const urlParams = new URLSearchParams(window.location.search);
 const token = urlParams.get('token');
 export const HOME_PAGE = '/'+token;
-export const REQUESTS_PAGE = '/requests';
+export const REQUESTS_PAGE = token+'/requests';
 export const BOOKINGS_PAGE = '/bookings';
 export const STAT_PAGE = '/stat';
