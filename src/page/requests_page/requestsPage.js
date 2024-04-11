1fc7f3de57b386eed4e03a599e6c77cc64661370
ibrohim74 <xibroxim@gmail.com>
2024-04-11 21:57:37 +0500

    test1213

--- src/page/requests_page/requestsPage.js
+++ src/page/requests_page/requestsPage.js
@@ -88,61 +88,76 @@ const RequestsPage = () => {
     useEffect(() => {
         getClient();
     }, [requests]);
-
+    console.log(dacha)
     return (
         <div className={`container`}>
             <HeaderPage url={BACK_HOME} title={'Заявки'}/>
             <div className={style.requests_list}>
-                {isLoading ? 'loading' : 'finished'}
-                {error}
-                {requests?.length > 0 ?
-                    requests.map((item) => {
-                        if (item.status === "awaiting") {
-                            return (
-                                <div className={style.sellerDashboard__new_request_item} key={item.id}>
-                                    <div className={style.sellerDashboard__new_request_item_column_1}>
-                                        <div className={style.sellerDashboard__new_request_item_column_photo}>
-                                            <Icon.ImgPlaceholder
-                                                width={"100%"}
-                                                height={"100%"}
-                                            />
+                {isLoading ? 'loading' : <>
+                    {requests?.length > 0 ?
+                        requests.map((item) => {
+                            if (item.status === "awaiting") {
+                                return (
+                                    <div className={style.sellerDashboard__new_request_item} key={item.id}>
+                                        {dacha.map((dachaItem , index)=>{
+                                            if (item?.accommodation_id === dachaItem.id) {
+                                                const currentPhotoUrl = photoUrls[index];
+                                                const currentClient = client.find(
+                                                    (clientItem) => clientItem.id === item.customer_id
+                                                );
+
+                                                return(
+<div>asdasd</div>
+                                                )
+                                            }
+                                        })}
+
+
+                                        <div className={style.sellerDashboard__new_request_item_column_1}>
+                                            <div className={style.sellerDashboard__new_request_item_column_photo}>
+                                                <Icon.ImgPlaceholder
+                                                    width={"100%"}
+                                                    height={"100%"}
+                                                />
+                                            </div>
+                                            <div className={style.sellerDashboard__new_request_item_column_title}>
+                                                <h1>Luex Dacha Premium</h1>
+                                                <div className={style.sellerDashboard__new_request_item_column_rating}>
+                                                    <p>sadasdasd </p>
+                                                    <Score score={3}/>
+                                                </div>
+                                            </div>
                                         </div>
-                                        <div className={style.sellerDashboard__new_request_item_column_title}>
-                                            <h1>Luex Dacha Premium</h1>
-                                            <div className={style.sellerDashboard__new_request_item_column_rating}>
-                                                <p>sadasdasd </p>
-                                                <Score score={3}/>
+
+                                        <div className={style.sellerDashboard__new_request_item_column_2}>
+                                            <div className={style.sellerDashboard__new_request_item_column_2_day}>
+                                                <p><Icon.Month/>Mon 05/12</p>
+                                                <p><Icon.Month/>Mon 05/12</p>
+                                            </div>
+                                        </div>
+
+                                        <div className={style.sellerDashboard__new_request_item_column_price}>
+                                            <h1>Цена:</h1>
+                                            <h1>200.000 Сум</h1>
+                                        </div>
+
+                                        <div className={style.sellerDashboard__new_request_item_column_3}>
+                                            <div className={style.sellerDashboard__new_request_item_column_3_buttons}>
+                                                <button onClick={() => acceptRequest(item.id)}>Принять</button>
+                                                <button onClick={() => denyRequest(item.id)}>Отказать</button>
                                             </div>
                                         </div>
                                     </div>
+                                );
+                            }
+                        }) :
+                        <div className={style.SellerDashboardNoData}>
+                            <Icon.NoDocuments/>
+                            <p>На данный момент ничего нету</p>
+                        </div>
+                    }
+                </> }
 
-                                    <div className={style.sellerDashboard__new_request_item_column_2}>
-                                        <div className={style.sellerDashboard__new_request_item_column_2_day}>
-                                            <p><Icon.Month/>Mon 05/12</p>
-                                            <p><Icon.Month/>Mon 05/12</p>
-                                        </div>
-                                    </div>
-
-                                    <div className={style.sellerDashboard__new_request_item_column_price}>
-                                        <h1>Цена:</h1>
-                                        <h1>200.000 Сум</h1>
-                                    </div>
-
-                                    <div className={style.sellerDashboard__new_request_item_column_3}>
-                                        <div className={style.sellerDashboard__new_request_item_column_3_buttons}>
-                                            <button onClick={() => acceptRequest(item.id)}>Принять</button>
-                                            <button onClick={() => denyRequest(item.id)}>Отказать</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            );
-                        }
-                    }) :
-                    <div className={style.SellerDashboardNoData}>
-                        <Icon.NoDocuments/>
-                        <p>На данный момент ничего нету</p>
-                    </div>
-                }
             </div>
         </div>
     );
--- src/page/stat_page/statPage.js
+++ src/page/stat_page/statPage.js
@@ -2,14 +2,16 @@ import React from 'react';
 import {BACK_HOME} from "../../utils/consts";
 import HeaderPage from "../../component/header_page/headerPage";
 import style from './stat.module.css'
+import Header from "../../component/header/header";
 
 const StatPage = () => {
     const currentDate = new Date();
-    const monthNames = ['январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь'];
+    const monthNames = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
     const currentMonthName = monthNames[currentDate.getMonth()];
 
     return (
         <div className={'container'}>
+            <Header/>
             <HeaderPage url={BACK_HOME} title={'Статистика'}/>
             <div className={style.stat_month}>
                 <select defaultValue={currentMonthName}>
@@ -19,6 +21,29 @@ const StatPage = () => {
                         </option>
                     ))}
                 </select>
+            </div>
+
+            <div className={style.stat_list}>
+                <div className={style.stat_list_item}>
+                    <p>Заработанно:</p>
+                    <p>1 200 000 Сум</p>
+                </div>
+                <div className={style.stat_list_item}>
+                    <p>Всего заявок:</p>
+                    <p>23</p>
+                </div>
+                <div className={style.stat_list_item}>
+                    <p>Успешные сделки:</p>
+                    <p>43</p>
+                </div>
+                <div className={style.stat_list_item}>
+                    <p>Отменённых бронирований:</p>
+                    <p>12</p>
+                </div>
+                <div className={style.stat_list_item}>
+                    <p>Преждевременных выселений:</p>
+                    <p>2</p>
+                </div>
             </div>
         </div>
     );
